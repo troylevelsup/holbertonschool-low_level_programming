c515931461f03f36aa20703d08349cd2fcadcd88
Troy Enns <troy@zerodbglobal.com>
2020-02-06 05:13:15 +0000

    added 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - print string to std error
+*
+* return:1
+*/
+
+int main(void)
+{
+	fprintf( stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
