d410f153fae34e81cd0a7d9185781977adb9f579
Troy Enns <troy@zerodbglobal.com>
2020-02-06 21:10:32 +0000

    betty'd 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,11 +20,11 @@ int main(void)
 
 	int last = n % 10;
 
-	if (last<0)
+	if (last < 0)
 	{
-		last=last*-1;
+		last = last * -1;
 	}
-	
+
 	if (last > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last);
@@ -34,8 +34,9 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, last);
 	}
 	else if (last < 6 && last != 0)
-	{	
+	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	}
 	return (0);
 }
+
