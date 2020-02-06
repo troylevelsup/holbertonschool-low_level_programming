845c403500f7529cc7b6c0d1a5aac6bcf1785ca7
Troy Enns <troy@zerodbglobal.com>
2020-02-06 20:47:26 +0000

    added negative usecase

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,6 +20,11 @@ int main(void)
 
 	int last = n % 10;
 
+	if (last<0)
+	{
+		last=last*-1;
+	}
+	
 	if (last > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last);
@@ -29,7 +34,8 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, last);
 	}
 	else if (last < 6 && last != 0)
+	{	
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
-
+	}
 	return (0);
 }
