58ef9ecf21d104f273fcf983f88efc59edfa0f8b
Troy Enns <troy@zerodbglobal.com>
2020-02-06 03:31:36 +0000

    linted 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main()
+/**
+* main - entry point
+*
+* Return: always 0
+*/
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	return (0);
 }
