18406d3fc9df6f035669bf61fcb7bcadc42bae7a
Troy Enns <troy@zerodbglobal.com>
2020-02-07 05:26:13 +0000

    added 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - prints the alphabet in lowercase
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
