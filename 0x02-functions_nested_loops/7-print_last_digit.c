c8f1669eb37ab651d6dcb826b7d4000a4279f3cf
Troy Enns <troy@zerodbglobal.com>
2020-02-11 22:02:43 +0000

    added 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* print_last_digit()- prints last digit
+* @num:starting number
+* Return: last digit of num
+*/
+
+
+int print_last_digit(int num)
+{
+	int result;
+
+	result = num % 10;
+	return (result);
+
+}
