77a1ce98054d2cc707d034b092f4c4819712a194
Troy Enns <troy@zerodbglobal.com>
2020-02-11 19:22:58 +0000

    ditched loop on 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,13 +8,15 @@
 
 int main(void)
 {
-	int i;
-	char holb[9] = "Holberton";
-
-	for (i = 0; i <= 9; i++)
-	{
-		_putchar(holb[i]);
-	}
+	_putchar('H');
+        _putchar('o');
+        _putchar('l');
+        _putchar('b');
+        _putchar('e');
+        _putchar('r');
+        _putchar('t');
+        _putchar('o');
+        _putchar('n');
 	_putchar('\n');
 	return (0);
 }
