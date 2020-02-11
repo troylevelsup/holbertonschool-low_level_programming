18e59333333d7b8c05294063039342d4704bad1a
Troy Enns <troy@zerodbglobal.com>
2020-02-11 21:27:49 +0000

    added 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+* print_sign- determines polarity of a number
+* return: 0 if 0, 1 if positive, and -1 if less than zero
+* @n: number print_sign checks
+*/
+
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
