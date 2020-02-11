f7c9e8f60314b255b01801f25706c1cec36e1fb5
Troy Enns <troy@zerodbglobal.com>
2020-02-11 19:53:29 +0000

    betty'd 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 
 /**
-* main- entry point
-* Return-0
+* print_alphabet- prints alphabet
+* Return : 0
 */
 
-void print_alphabet (void){
+void print_alphabet(void)
+{
 	int i;
-	
+
 	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
-	
+
 	_putchar('\n');
 }
