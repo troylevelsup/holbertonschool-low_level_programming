785246c5a7f505fde3377cbc758de21996d4d17a
Troy Enns <troy@zerodbglobal.com>
2020-02-11 21:06:03 +0000

    fixed 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,17 +2,14 @@
 
 /**
 * _islower- checks for lowercase character
+* @c: int that _islower checks
 * Return: 1 if lowercase, 0 otherwise
 */
 
 int _islower(int c)
 {
-	int i;
-
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		if (c == i)
-			return (1);
-	}
-	return (0);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
