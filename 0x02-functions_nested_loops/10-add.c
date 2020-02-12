c4127eba16f63545c6f1e1d399c6106f98801405
Troy Enns <troy@zerodbglobal.com>
2020-02-12 03:48:03 +0000

    added 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* add()- adds two numbers
+* Return: sum of two numbers
+* @x: first number
+* @y: second number
+*/
+
+int add(int x, int y)
+{
+	int sum = x + y;
+
+	return (sum);
+}
