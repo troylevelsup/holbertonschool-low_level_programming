0493d910d6ee0a8dd10efb6f53c0aad196df4d23
Troy Enns <troy@zerodbglobal.com>
2020-02-13 21:30:17 +0000

    added 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* print_line - prints a line n number of times
+* Return: void
+* @n: length of line
+*/
+
+void print_line(int n)
+{
+	int i;
+	for (i = 0; i <= n; i++)
+	{
+		if (n > 0)
+		_putchar('_');
+	}
+	_putchar('\n');
+}
