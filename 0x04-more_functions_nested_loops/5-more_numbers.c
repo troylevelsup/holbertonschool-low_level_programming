d2e02c4ce54b0a3996c2ee90720d81aecdd6794f
Troy Enns <troy@zerodbglobal.com>
2020-02-13 21:10:34 +0000

    lost a putchar on 3

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,8 @@
 #include "holberton.h"
+
 /**
 * more_numbers - prints 10 x 0-14 followed by a newline
+*
 * Return: nothing
 */
 
@@ -12,16 +14,11 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j < 10)
-				_putchar(j + '0');
-			else
-			{
-				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
-			}
+			if (j > 10)
+				_putchar(j / 10 + '0');
+			_putchar((j % 10) + '0');
+		
 		}
 	_putchar('\n');
 	}
 }
-
-
