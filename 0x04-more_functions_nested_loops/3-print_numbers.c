463c1538206d81ef98d792671d5b7abbb1f60d72
Troy Enns <troy@zerodbglobal.com>
2020-02-13 20:28:19 +0000

    betty'd 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 /**
-* print_numbers - prints numbers 0 to 9
-* return: nothing
+* print_numbers - prints 0-9.
+* Return: nothing
 */
 
 void print_numbers(void)
 {
 	int i;
-	for(i = 0; i <= 9;i++)
+
+	for (i = 0; i <= 9; i++)
 	{
 		_putchar(i + '0');
 	}
-	_putchar("/n");
+	_putchar('\n');
 }
