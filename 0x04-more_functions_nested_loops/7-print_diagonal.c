faca24c7a5e5b5889a706b3503e6e2379fb57c71
Troy Enns <troy@zerodbglobal.com>
2020-02-14 18:25:29 +0000

    fixed 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,11 +10,18 @@
 void print_diagonal(int n)
 {
 	int i, j;
-	for (i=0; i <= n; i++)
+
+	if (n < 0)
 	{
-		if(i >= 1)
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		if (i > 0)
 		{
-			for (j = 0; j<= i; j++)
+			for (j = 0; j < i; j++)
 			{
 				_putchar(' ');
 			}
